11f6b445ce999c3dc452a03b09cdee36e7483966
deltaisfordeath <kennyheagle@gmail.com>
2023-05-07 08:06:31 -0700

    resolve dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="app-container">
         <div className='app-header'>Welcome to Kenny Heagle's Page!</div>
+        <h5>I'm still figuring this stuff out...</h5>
     </div>
   );
 }
