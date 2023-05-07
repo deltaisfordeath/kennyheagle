94d553ce501739ac39aadc1a0a40f2d46c4590a3
deltaisfordeath <kennyheagle@gmail.com>
2023-05-07 07:55:44 -0700

    initialize

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app-container">
+        <div className='app-header'>Welcome to Kenny Heagle's Page!</div>
+    </div>
+  );
+}
+
+export default App;
