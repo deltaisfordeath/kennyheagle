eac276a18bd77216e7a3701691288c0bd80d7824
deltaisfordeath <kennyheagle@gmail.com>
2023-09-01 07:29:41 -0700

    added custom logo/icon

--- src/App.tsx
+++ src/App.tsx
@@ -61,9 +61,7 @@ function App() {
   return (
     <div className={`app-container ${theme}`}>
       <div className='app-topbar'>
-        <div className='app-topbar-left'>
-          <div className='app-header'><span className={`kenny-heagle-logo ${theme}`}>{'<>'}</span>Kenny Heagle<span className={`kenny-heagle-logo ${theme}`}>{'</>'}</span></div>
-        </div>
+        <div className='app-header'>Kenny <div className={`kenny-heagle-logo ${theme}`} /> Heagle</div>
         <div className='app-theme-icon' onClick={toggleTheme}>{theme === 'light' ? <LightModeIcon /> : <DarkModeIcon />}</div>
         <div id='nav-menu-button' className="app-navigation-menu-button" onClick={toggleNavMenu}>
             <MenuIcon sx={{pointerEvents: 'none'}} />
